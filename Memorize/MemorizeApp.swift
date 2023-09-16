3d4d77f56018eee2237c0c1c3941d71d7bb68d4d
Giuliano Machado <giuliano.machado@mindera.com>
2023-09-16 13:29:43 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Giuliano Machado on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
