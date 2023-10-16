3b2e025b20d24faca7176964fbc2e6744f481925
Giuliano Machado <giuliano.machado@mindera.com>
2023-10-16 21:59:55 +0100

    finished required task 1 of assignement 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,39 +14,56 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            HStack {
-                // ForEach View also takes parameters, like index in
-                // Array.indices === array.length
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView {
+                cards
             }
-             .foregroundColor(.orange)
-
-            HStack {
-                Button(action: {
-                    cardCount -= 1
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.minus.fill")
-                })
-                .imageScale(.large)
-                .font(.largeTitle)
-                // View to add space
-                Spacer()
-                Button(action: {
-                    cardCount += 1
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.plus.fill")
-                })
-                .imageScale(.large)
-                .font(.largeTitle)
-            }
-
+            Spacer()
+            cardCountAdjusters
         }
-
-
-        .imageScale(.small)
         .padding()
+    }
+
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            // ForEach View also takes parameters, like index in
+            // Array.indices === array.length
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            // View to add space
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+
+    // External names and internal names: `by` is used when calling the function,
+    // `offset` is used internally in the function.
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+
+    var cardRemover: some View {
+        // When theres one line of code you can return the value implicitly
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+
+    var cardAdder: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -56,20 +73,22 @@ struct CardView: View {
     // if they are not constant.
     //  @State creates a pointer in memory to store isFaceUp
     // `Temporary state`
-    @State var isFaceUp = false
+    @State var isFaceUp = true
 
 
     var body: some View {
-        let base = RoundedRectangle(cornerRadius: 12)
-
         ZStack {
-            if (isFaceUp) {
-                base.foregroundColor(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            } else {
-                base
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                if (isFaceUp) {
+                    base.foregroundColor(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
             }
+            .opacity(isFaceUp ? 1 : 0)
+            // Opacity so that the element is rendered in its proper height
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             // Structs have funtions in them, e.g. Bool has `toggle`
